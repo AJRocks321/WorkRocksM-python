5355794eb83a52ec5f77c00867798db573b12fa8
AJRocks123M <70801883+AJRocks123M@users.noreply.github.com>
2020-09-05 16:13:46 +0530

    chaos game
    graph.py is a scatter plot points for chaos game. you can get an output of many points, which you can copy and paste on notepad and save in .dat format and plot it using gnuplot. you will surely like it.
    here is link for wikipedia page on this topic:  https://en.wikipedia.org/wiki/Chaos_game

---
+++ graph.py
@@ -0,0 +1,35 @@
+import random
+import math
+
+p=3.14159265
+a=float(input("give number of sides you want (believe me, only odd are gonna work)"))
+b=float(input("also type size for graph"))
+m=2*p/a #per sector
+c1=float(input("give special point"))
+c2=float(input())
+n=int(input("number of output points"))
+
+t=int(a)
+sin=[]
+cos=[]
+for i in range(t):
+    sin.append(float(math.sin(i*m)))
+    cos.append(float(math.cos(i*m)))
+
+#coordinates of main polygon
+x=[]
+y=[]
+for j in range(t):
+    x.append(float(b*cos[j]))
+    y.append(float(b*sin[j]))
+
+#there are n points* 01234..(n-1)
+for k in range(n):
+    h=random.randint(0,t)
+    for l in range(t):
+    	if (h==l):
+    		c1=float(c1+x[l])/2
+    		c2=float(c2+y[l])/2
+    		print(c1," ",c2)
+    		break
+    
